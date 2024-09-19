9b8b29588522d7f5af2cac58d709a5e7146119d8
luisadan22 <zs23024135@estudiantes.uv.mx>
2024-09-19 14:52:08 -0600

    practica05

--- practica05.js
+++ practica05.js
@@ -5,24 +5,30 @@ const Picture =parametros => {
     const src= url+parametros+size
     return `
     <figure>
-        <img src="https://picsum.photos/id/${parametros}/200/200" alt="" id="${parametros}">
+        <img src="https://picsum.photos/id/${parametros}/200/200" alt="" id="${parametros}" class="">
     <figcaption >${parametros}</figcaption>
  </figure>
  `
 }
 console.log(Picture())
-app.innerHTML=Picture(1)+Picture(2)+Picture(3)+Picture(4)
+app.innerHTML=Picture(1)+Picture(2)+Picture(3)
 
-const elemento1 = document.getElementById("a1")
-const elemento2 = document.getElementById("a2")
-const elemento3 = document.getElementById("a3")
+//const elemento1 = document.getElementById("a1")
+//const elemento2 = document.getElementById("a2")
+//const elemento3 = document.getElementById("a3")
 
-elemento1.addEventListener("click", mifuncion)
-elemento2.addEventListener("click", mifuncion)
-elemento3.addEventListener("click", mifuncion)
+//elemento1.addEventListener("click", mifuncion)
+//elemento2.addEventListener("click", mifuncion)
+//elemento3.addEventListener("click", mifuncion)
 
-
-function mifuncion(parametros){
+const lista =document.querySelectorAll("img")
+for (let i = 0; i < lista.length; i++) {
+lista[i].addEventListener("mouseenter",(evento) => {evento.target.classList.toggle("redonda")})
+}
+function mifuncion(evento){
     console.log("ahyyy: ")
-    console.log(parametros)
+    console.log(evento)
+    var imagen = evento.target
+    imagen.classList.toggle("redonda")
+
 }
